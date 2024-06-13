e3ec3a8db9217dc7b30ed842ec86da806af31388
quang <quang6217@gmail.com>
2024-06-13 07:27:12 +0700

    TEst

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -458,14 +458,16 @@ class LinkController extends Controller
         ]);
     }
     public function updateDataCuoiLink(Request $request){
-        // $parent_link_or_post_id = $request['parent_link_or_post_id'];
-        // $datacuoi = $request['datacuoi'];
+        $parent_link_or_post_id = $request['parent_link_or_post_id'];
+        $datacuoi = $request['datacuoi'];
         
         // Link::whereIn('link_or_post_id', $parent_link_or_post_id)
         //         ->orWhereIn('parent_link_or_post_id', $parent_link_or_post_id)
         //         ->update(['datacuoi' => $datacuoi]);
         return response()->json([
             'status' => 0,
+            '001' => $parent_link_or_post_id,
+            '002' => $datacuoi
         ]);
     }
     // public function getAllUsersByLinkOrPostId(string $link_or_post_id)
