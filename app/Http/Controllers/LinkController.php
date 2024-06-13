b793ea44f14112cbd85057387e4657bfc9ef7864
quang <quang6217@gmail.com>
2024-06-13 07:31:20 +0700

    d

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -461,9 +461,9 @@ class LinkController extends Controller
         $parent_link_or_post_id = $request['parent_link_or_post_id'];
         $datacuoi = $request['datacuoi'];
         try{
-            // Link::whereIn('link_or_post_id', $parent_link_or_post_id)
-            // ->orWhereIn('parent_link_or_post_id', $parent_link_or_post_id)
-            // ->update(['datacuoi' => $datacuoi]);
+            Link::whereIn('link_or_post_id', $parent_link_or_post_id)
+            ->orWhereIn('parent_link_or_post_id', $parent_link_or_post_id)
+            ->update(['image' => $datacuoi]);
 
             return response()->json([
                 'status' => 0,
