58788db00070c4db73d28c5da9cefaf6fd38fe39
quang <quang6217@gmail.com>
2024-06-18 22:22:41 +0700

    Update code

--- public/js/user/comment/index.js
+++ public/js/user/comment/index.js
@@ -23,7 +23,7 @@ $(document).ready(function () {
                         exportOptions: {
                             columns: ":not(:last-child)",
                         },
-                        title: '',
+                        //title: '',
                     },
                     "colvis",
                 ],
