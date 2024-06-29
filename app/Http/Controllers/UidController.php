81ba18fc652678b872811802945af06c6271c869
quang <quang6217@gmail.com>
2024-06-29 15:08:02 +0700

    Add phone

--- app/Http/Controllers/UidController.php
+++ app/Http/Controllers/UidController.php
@@ -10,35 +10,21 @@ class UidController extends Controller
     public function uploadUid(Request $request)
     {
         try {
-            $request->validate([
-                'file' =>  'required|mimes:json',
+            $data = $request->validate([
+                'phone' => 'nullable|array',
+                'phone.*.uid' => 'required|string',
+                'phone.*.phone' => 'nullable|string',
             ]);
-            $file_name = $request->file('file')->getClientOriginalName();
-            $pathFull = 'upload/';
-            $request->file('file')->storeAs(
-                'public/' . $pathFull,
-                $file_name
-            );
-            $content = json_decode(file_get_contents(storage_path('app/public/upload/' . $file_name)), true);
-            $data = array_map(function ($item) {
-                return [
-                    'uid' => $item['uid'],
-                    'phone' => !empty($item['newphone']) ?
-                        $item['phone'] . ',' . $item['newphone'] : $item['phone'],
-                ];
-            }, $content);
-            Uid::upsert($data, [
-                'uid'
-            ], ['phone']);
+            return response()->json([
+                'status' => 0,
+                'data' => $data
+            ]);
+            //Uid::upsert($data, ['uid'], ['phone']);
         } catch (Throwable $e) {
             return response()->json([
                 'status' => 1,
                 'message' => $e->getMessage()
             ]);
         }
-
-        return response()->json([
-            'status' => 0,
-        ]);
     }
 }
