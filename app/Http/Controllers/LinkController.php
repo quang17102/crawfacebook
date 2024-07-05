a6ac86291ee6e0d3f882a0bca86027ad8e2ca94f
quang <quang6217@gmail.com>
2024-07-05 22:47:39 +0700

    filter

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -427,14 +427,15 @@ class LinkController extends Controller
                         //Data cuoi
                         ->when(strlen($startDateTimeStr) || strlen($endDateTimeStr), function ($q) use ($startDateTimeStr, $endDateTimeStr) {
                             return $q->where(function ($query) use ($startDateTimeStr, $endDateTimeStr) {
-                                if (strlen($startDateTimeStr)) {
-                                    $query->where('datacuoi', '>=', $startDateTimeStr);
-                                }
-                                if (strlen($endDateTimeStr)) {
-                                    $query->orWhere('datacuoi', '>=', $endDateTimeStr);
-                                }
                                 if (strlen($startDateTimeStr) && strlen($endDateTimeStr)) {
                                     $query->where('datacuoi', '<=', $startDateTimeStr)->where('datacuoi', '>=',$endDateTimeStr);
+                                }else{
+                                    if (strlen($startDateTimeStr)) {
+                                        $query->where('datacuoi', '>=', $startDateTimeStr);
+                                    }
+                                    if (strlen($endDateTimeStr)) {
+                                        $query->orWhere('datacuoi', '>=', $endDateTimeStr);
+                                    }
                                 }
                             });
                         })
