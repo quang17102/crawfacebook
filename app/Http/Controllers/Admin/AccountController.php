8b7e40795e60d0b3b407443aa0caff8016365fca
quang <quang6217@gmail.com>
2024-07-21 23:37:49 +0700

    roles

--- app/Http/Controllers/Admin/AccountController.php
+++ app/Http/Controllers/Admin/AccountController.php
@@ -36,7 +36,7 @@ class AccountController extends Controller
                 'expire' => 'required|integer',
                 'role' => 'required|in:0,1',
                 'roles' => 'nullable|array',
-                'roles.*' => 'nullable|integer|in:0,1,2,3,4,5,6',
+                'roles.*' => 'nullable|integer|in:0,1,2,3,4,5,6,7',
             ]);
             $check = User::firstWhere('name', $data['name']);
             if ($check) {
@@ -146,7 +146,7 @@ class AccountController extends Controller
             Toastr::success(__('message.success.update'), __('title.toastr.success'));
         } catch (Throwable $e) {
             DB::rollBack();
-            Toastr::error($e->getMessage(), __('title.toastr.fail'));
+            Toastr::error($e->getMessage(), __('title.toastr.fail'. $e->getMessage()));
         }
 
         return redirect()->back();
