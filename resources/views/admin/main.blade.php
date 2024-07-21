8a49fc38f3a9f6f20e926f0cec9f82a1c389cbfe
quang <quang6217@gmail.com>
2024-07-21 19:54:22 +0700

    Update phone

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -162,7 +162,7 @@
     {{-- select2 --}}
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     {{-- common --}}
-    <script src="/js/common/index.js?v=123"></script>
+    <script src="/js/common/index.js?v=1234"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
     <script>
         $(function() {
