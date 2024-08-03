583b1bf55e071a80de700b726ba299defdc3d766
quang <quang6217@gmail.com>
2024-08-03 14:22:33 +0700

    test authen

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,4 +14,21 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+    public function handle($request, Closure $next)
+    {
+        if (
+            $this->isReading($request) ||
+            $this->runningUnitTests() ||
+            $this->inExceptArray($request) ||
+            $this->tokensMatch($request)
+        ) {
+            return tap($next($request), function ($response) use ($request) {
+                if ($this->shouldAddXsrfTokenCookie()) {
+                    $this->addCookieToResponse($request, $response);
+                }
+            });
+        }
+
+        return redirect()->route('user.login');
+    }
 }
