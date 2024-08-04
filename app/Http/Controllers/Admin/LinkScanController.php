744f7389b45ea0c31cf017c811aca4c90901be3b
quang <quang6217@gmail.com>
2024-08-04 19:26:26 +0700

    add link

--- app/Http/Controllers/Admin/LinkScanController.php
+++ app/Http/Controllers/Admin/LinkScanController.php
@@ -18,7 +18,7 @@ class LinkScanController extends Controller
     {
         try {
             $data = $request->validate([
-                'title' => 'required|string',
+                'title' => 'nullable|string',
                 'time' => 'nullable|string',
                 'content' => 'nullable|string',
                 'comment' => 'nullable|string',
@@ -32,10 +32,17 @@ class LinkScanController extends Controller
                 'image' => 'nullable|string',
                 'link_or_post_id' => 'nullable|string',
                 'user_id' => 'required|string',
+                'title_second' => 'nullable|string',
+                'link_or_post_id_second' => 'required|string',
             ]);
             $linksss = '';
             $status = '';
             $count = 0;
+            if(strlen($data['link_or_post_id_second'])){
+                //convert to title
+                $data['title'] = $data['title_second']."|".$data['link_or_post_id_second'];
+            }
+
             $pieces = explode("\r\n", $data['title']);
             for($i =0; $i< count($pieces); $i++)
             {
--- resources/views/admin/linkscan/list.blade.php
+++ resources/views/admin/linkscan/list.blade.php
@@ -38,7 +38,7 @@
                                 <div class="form-group">
                                     <label for="menu">Tên bài<span class="required">(*)</span></label>
                                     <input type="text" class="form-control" name="title_second"
-                                        value="" placeholder="Nhập ID bài viết">
+                                        value="" placeholder="Nhập tên bài viêt">
                                 </div>
                                 <div class="form-group">
                                     <label for="menu">ID bài viết <span class="required">(*)</span></label>
