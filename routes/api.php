12e0edddb269afabef066696e3a121b8bead4147
quang <quang6217@gmail.com>
2024-08-07 21:48:03 +0700

    Update paginationuser

--- routes/api.php
+++ routes/api.php
@@ -75,6 +75,9 @@ Route::group(['namespace' => 'App\Http\Controllers'], function () {
         Route::get('/getAllReactionUser', 'ReactionController@getAllReactionUser')->name('getAllReactionUser');
         Route::get('/getAllPagination', 'ReactionController@getAllPagination')->name('getAllPagination');
         Route::get('/getAllPaginationParam', 'ReactionController@getAllPaginationParam')->name('getAllPaginationParam');
+
+        Route::get('/getAllPaginationUser', 'ReactionController@getAllPaginationUser')->name('getAllPaginationUser');
+        Route::get('/getAllPaginationParamUser', 'ReactionController@getAllPaginationParamUser')->name('getAllPaginationParamUser');
     });
 
     #upload
