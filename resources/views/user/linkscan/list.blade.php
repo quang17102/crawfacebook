d39c511168128ebb7ed0a8c14d3704b479e48c39
quang <quang6217@gmail.com>
2024-08-27 15:01:25 +0700

    update role

--- resources/views/user/linkscan/list.blade.php
+++ resources/views/user/linkscan/list.blade.php
@@ -32,7 +32,7 @@
                                 <div class="form-group">
                                     <label for="menu">Tên bài<span class="required">(*)</span></label>
                                     <input type="text" class="form-control" name="title_second"
-                                        value="" placeholder="Nhập tên bài viêt">
+                                        value="{{$user["name"]}}" placeholder="Nhập tên bài viêt">
                                 </div>
                                 <div class="form-group">
                                     <label for="menu">ID bài viết <span class="required">(*)</span></label>
