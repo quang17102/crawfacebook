f877601afd22ca66953568ce8644734e8969a788
quang <quang6217@gmail.com>
2024-08-28 10:19:59 +0700

    Update code

--- app/Http/Controllers/Users/LinkFollowController.php
+++ app/Http/Controllers/Users/LinkFollowController.php
@@ -305,9 +305,21 @@ class LinkFollowController extends Controller
 
     public function getAll()
     {
+        $result = UserRole::where('user_id', Auth::id())->pluck('role')->toArray() ?? [];
+        $permistion_reaction = "NO";
+        $permistion_view = "NO";
+        if (in_array(7, $result)) {
+            $permistion_reaction = "YES";
+        } 
+        if (in_array(8, $result)) {
+            $permistion_view = "YES";
+        }
+
         return response()->json([
             'status' => 0,
-            'linkfollows' => Link::where('type', GlobalConstant::TYPE_FOLLOW)->get()
+            'linkfollows' => Link::where('type', GlobalConstant::TYPE_FOLLOW)->get(),
+            'permistion_reaction' => $permistion_reaction,
+            'permistion_view' => $permistion_view,
         ]);
     }
 }
