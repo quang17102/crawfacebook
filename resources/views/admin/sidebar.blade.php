c1cf371fe18da437273e99fc0c65efef5b893bb8
quang <quang6217@gmail.com>
2024-08-29 16:46:10 +0700

    Update code

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -190,7 +190,7 @@
                         </li>
                         <li
                             class="nav-item {{ in_array(request()->route()->getName(), ['settings_admin_2.index']) ? 'menu-is-opening menu-open' : '' }}">
-                            <a href="{{ route('admin/settingfilters.index') }}" class="nav-link">
+                            <a href="{{ route('/admin/settingfilters.index') }}" class="nav-link">
                                 <i class="nav-icon fa-solid fa-gear"></i>
                                 <p>
                                     Cài đặt Filter
