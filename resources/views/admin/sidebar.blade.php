df79434ced6de832b16d173a7d17fe6bcdb07b4c
quang <quang6217@gmail.com>
2024-08-29 16:48:59 +0700

    Update code

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -188,7 +188,7 @@
                                 </p>
                             </a>
                         </li>
-                        <li
+                        <!-- <li
                             class="nav-item {{ in_array(request()->route()->getName(), ['settings_admin_2.index']) ? 'menu-is-opening menu-open' : '' }}">
                             <a href="{{ route('settings_admin_2.index') }}" class="nav-link">
                                 <i class="nav-icon fa-solid fa-gear"></i>
@@ -196,7 +196,7 @@
                                     Cài đặt Filter
                                 </p>
                             </a>
-                        </li>
+                        </li> -->
                     @break
 
                 @endswitch
