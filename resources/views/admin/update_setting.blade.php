b5200aafb9cde0ecac1302c8f10e5a781f8832e7
quang <quang6217@gmail.com>
2024-08-29 21:44:53 +0700

    Update code

--- resources/views/admin/update_setting.blade.php
+++ resources/views/admin/update_setting.blade.php
@@ -15,10 +15,112 @@
 @endpush
 @section('content')
     <form action="{{ route('admin.settingfilters.update', ['id' => $user->id]) }}" method="POST">
-        <div class="card-body">
-        </div>
-        <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Lưu</button>
+    <div class="row">
+            <div class="col-lg-12">
+                <div class="card direct-chat direct-chat-primary">
+                    <div class="card-header ui-sortable-handle header-color" style="cursor: move;">
+                        <h3 class="card-title text-bold">Thêm setting</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body" style="display: block;padding: 10px !important;">
+                        <div class="row">
+                            <div class="form-group col-2">
+                                <label for="menu">Data cuối từ<span class=""></span></label>
+                                <input type="text" class="form-control" name="data_cuoi_from"
+                                    value="{{$settingfilter->data_cuoi_from}}" placeholder="">
+                            </div>
+                            <div class="form-group col-2">
+                                <label for="menu">Data cuối đến<span class=""></span></label>
+                                <input type="text" class="form-control" name="data_cuoi_to"
+                                    value="{{$settingfilter->data_cuoi_to}}" placeholder="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-2">
+                                <label for="menu">Reaction chênh từ<span class=""></span></label>
+                                <input type="text" class="form-control" name="reaction_chenh_from"
+                                    value="{{$settingfilter->reaction_chenh_from}}" placeholder="">
+                            </div>
+                            <div class="form-group col-2">
+                                <label for="menu">Reaction chêng đến<span class=""></span></label>
+                                <input type="text" class="form-control" name="reaction_chenh_to"
+                                    value="{{$settingfilter->reaction_chenh_to}}" placeholder="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-2">
+                                <label for="menu">Data Reaction chênh từ<span class=""></span></label>
+                                <input type="text" class="form-control" name="data_reaction_chenh_from"
+                                    value="{{$settingfilter->data_reaction_chenh_from}}" placeholder="">
+                            </div>
+                            <div class="form-group col-2">
+                                <label for="menu">Data Reaction chêng đến<span class=""></span></label>
+                                <input type="text" class="form-control" name="data_reaction_chenh_to"
+                                    value="{{$settingfilter->data_reaction_chenh_to}}" placeholder="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-2">
+                                <label for="menu">Comment chênh từ<span class=""></span></label>
+                                <input type="text" class="form-control" name="comment_chenh_from"
+                                    value="{{$settingfilter->comment_chenh_from}}" placeholder="">
+                            </div>
+                            <div class="form-group col-2">
+                                <label for="menu">Comment chênh đến<span class=""></span></label>
+                                <input type="text" class="form-control" name="comment_chenh_to"
+                                    value="{{$settingfilter->comment_chenh_to}}" placeholder="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-2">
+                                <label for="menu">Data Comment chênh từ<span class=""></span></label>
+                                <input type="text" class="form-control" name="data_comment_chenh_from"
+                                    value="{{$settingfilter->data_comment_chenh_from}}" placeholder="">
+                            </div>
+                            <div class="form-group col-2">
+                                <label for="menu">Data Comment chêng đến<span class=""></span></label>
+                                <input type="text" class="form-control" name="data_comment_chenh_to"
+                                    value="{{$settingfilter->data_comment_chenh_to}}" placeholder="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-2">
+                                <label for="menu">View chênh từ<span class=""></span></label>
+                                <input type="text" class="form-control" name="view_chenh_from"
+                                    value="{{$settingfilter->view_chenh_from}}" placeholder="">
+                            </div>
+                            <div class="form-group col-2">
+                                <label for="menu">View chêng đến<span class=""></span></label>
+                                <input type="text" class="form-control" name="view_chenh_to"
+                                    value="{{$settingfilter->view_chenh_to}}" placeholder="">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group  col-2">
+                                <label for="menu">Delay<span class=""></span></label>
+                                <input type="text" class="form-control" name="delay"
+                                    value="{{$settingfilter->delay}}" placeholder="">
+                            </div>
+                            <div class="form-group col-2">
+                                    <label for="menu">Người dùng <span class="required">(*)</span></label>
+                                    <select name="status" class="form-control">
+                                        <option value="ON" {{ $$settingfilter->status == 'ON' ? 'selected' : '' }}>ON</option>
+                                        <option value="OFF" {{ $$settingfilter->status == 'OFF' ? 'selected' : '' }}>OFF</option>
+                                        <option value="FOLLOWTOSCAN" {{ $$settingfilter->status == 'FOLLOWTOSCAN' ? 'selected' : '' }}>FOLLOW -> SCAN</option>
+                                        <option value="SCANTOFOLLOW" {{ $$settingfilter->status == 'SCANTOFOLLOW' ? 'selected' : '' }}>SCAN -> FOLLOW</option>
+                                    </select>
+                                </div>
+                        </div>
+                    </div>
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Lưu</button>
+                    </div>
+                </div>
+            </div>
         </div>
         @csrf
     </form>
