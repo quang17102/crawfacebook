b52ceb733661a154ffcbf9627c24afbdc87cacfa
quang <quang6217@gmail.com>
2024-09-05 09:41:07 +0700

    Add api

---
+++ app/Http/Controllers/SettingFilterController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\SettingFilter;
+use App\Models\UserRole;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\DB;
+use Throwable;
+use Toastr;
+class SettingFilterController extends Controller
+{    
+    public function getAll()
+    {
+        return response()->json([
+            'status' => 0,
+            'settings' => SettingFilter::orderBy('id')->get()
+        ]);
+    }
+}
