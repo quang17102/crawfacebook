e78be8974388cdc35586f7930f31fbb9c0b715c4
Quang Nguyen <Quang.Nguyen@lr.org>
2024-11-18 14:48:58 +0700

    Update code

--- app/Http/Controllers/Admin/SettingFilterController.php
+++ app/Http/Controllers/Admin/SettingFilterController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Controller;
 use App\Models\SettingFilter;
 use App\Models\UserRole;
 use App\Models\User;
+use App\Constant\GlobalConstant;
 use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
