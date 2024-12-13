089355e6cb0f09fedeea5175b8bb0f662e6c8310
Quang Nguyen <Quang.Nguyen@lr.org>
2024-12-13 23:45:04 +0700

    Update filter

--- app/Http/Controllers/LinkController.php
+++ app/Http/Controllers/LinkController.php
@@ -458,7 +458,7 @@ class LinkController extends Controller
                                         $query->orWhere('datacuoi', '>=', $endDateTimeStr);
                                     }
                                 }
-                                if ($endDateTimeStr == 9999) {
+                                if ($last_data_to == 9999) {
                                     $query->orWhereNull('datacuoi');
                                 }
                             });
